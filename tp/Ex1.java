3f98bd3c8f314516bb79008a9d5d1dacb15ab319
Gabriel Charib <110329448+gabrielCharibPolls@users.noreply.github.com>
2023-10-04 22:32:28 +0200

    ex1 finished

---
+++ tp/Ex1.java
@@ -0,0 +1,29 @@
+package tp;
+import java.io.IOException;
+import java.net.InetAddress;
+
+///////////////////////////////////////////////////////////////////////////////
+///pour avoir les adres host name ou host adress  il faut utilise inet adress
+//////////////////////////////////////////////////////////////////////////////
+
+
+public class Ex1 {
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+////quand on utilise ce genre de methode ça peut declacher une erreur donc il faut utilise IOExeption
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public static void main(String[] args) throws IOException {
+    log("google.be", InetAddress.getByName("google.be"));
+    log("8.8.8.8", InetAddress.getByAddress(new byte[] {8,8,8,8}));
+}
+
+
+private static void log (String sAdres_search, InetAddress a_Address){
+    System.out.println("Search terme"+sAdres_search+"':");
+    System.out.println("gethostneameAdress() = "+ a_Address.getHostAddress()+"");
+    System.out.println("getHostName() == " + a_Address.getHostName());
+    System.out.println();
+}
+
+}
